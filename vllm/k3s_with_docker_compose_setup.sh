e337954a9957dc493d58ce8888cec59546351783
Jain Wun Su <johnny12150@gmail.com>
2026-01-10 01:23:27 +0800

    Fix yaml path

--- vllm/k3s_with_docker_compose_setup.sh
+++ vllm/k3s_with_docker_compose_setup.sh
@@ -1,5 +1,5 @@
 # Deploy
-kubectl apply -f yml/vllm/cpu_deployment.yaml \
+kubectl apply -f vllm/cpu_deployment.yaml \
         --kubeconfig docker/k3s_with_docker_compose/output/kubeconfig.yaml
-kubectl apply -f yml/vllm/svc.yaml \
+kubectl apply -f vllm/svc.yaml \
         --kubeconfig docker/k3s_with_docker_compose/output/kubeconfig.yaml
