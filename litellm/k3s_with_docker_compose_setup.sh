3c9454486c1cbe4ea803428884383a0184b59e8d
Jain Wun Su <johnny12150@gmail.com>
2026-01-10 01:18:14 +0800

    Fix yaml path

--- litellm/k3s_with_docker_compose_setup.sh
+++ litellm/k3s_with_docker_compose_setup.sh
@@ -1,5 +1,5 @@
 # Deploy
-kubectl apply -f yml/litellm/deployment.yaml \
+kubectl apply -f litellm/deployment.yaml \
         --kubeconfig docker/k3s_with_docker_compose/output/kubeconfig.yaml
-kubectl apply -f yml/litellm/svc.yaml \
+kubectl apply -f litellm/svc.yaml \
         --kubeconfig docker/k3s_with_docker_compose/output/kubeconfig.yaml
