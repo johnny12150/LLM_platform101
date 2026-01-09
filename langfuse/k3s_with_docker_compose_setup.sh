7f4a97d0b996cdc1607103c6d24708288c846813
Jain Wun Su <johnny12150@gmail.com>
2026-01-10 01:15:16 +0800

    Fix shell script
    Removed commented upgrade and uninstall commands, and updated svc.yaml path.

--- langfuse/k3s_with_docker_compose_setup.sh
+++ langfuse/k3s_with_docker_compose_setup.sh
@@ -6,16 +6,5 @@ helm install langfuse langfuse/langfuse \
         --namespace llm-platform \
         --kubeconfig docker/k3s_with_docker_compose/output/kubeconfig.yaml \
         -f yml/langfuse/values.yaml
-kubectl apply -f yml/langfuse/svc.yaml \
+kubectl apply -f langfuse/svc.yaml \
         --kubeconfig docker/k3s_with_docker_compose/output/kubeconfig.yaml
-
-## Upgrade
-#helm upgrade langfuse langfuse/langfuse \
-#        --namespace llm-platform \
-#        --kubeconfig docker/k3s_with_docker_compose/output/kubeconfig.yaml \
-#        -f yml/langfuse/values.yaml
-
-## Uninstall
-#helm uninstall langfuse \
-#        --namespace llm-platform \
-#        --kubeconfig docker/k3s_with_docker_compose/output/kubeconfig.yaml
